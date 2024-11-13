e7334595f1854e8b61148d2c3faffa51754c72c0
Kaveen Uththara <it21108990@my.sliit.lk>
2024-11-13 12:40:41 +0530

    first

---
+++ frontend/src/app/page.js
@@ -0,0 +1,12 @@
+import PaymentButton from "@/components/PaymentButton";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="App">
+      <h1>PayHere Payment Integration</h1>
+      <PaymentButton />
+    </div>
+    </div>
+  );
+}
